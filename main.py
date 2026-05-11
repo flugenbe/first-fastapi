3dca14870ca5b035d75e2499bca6b7be3bc62820
lugenbfd <93534634+lugenbfd@users.noreply.github.com>
2026-05-11 11:25:16 -0400

    version 3

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text: str
+    is_done: bool = False
 
 items = []
 
@@ -9,12 +14,16 @@ def root():
     return {"Hello": "World"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
